808843e840122d08d00bec1723ed4cd1b20fed81
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-01-30 12:00:44 +1100

    add v0.8

---
+++ v0.8.5/generated/twodnavierstokes_decaying.jl
@@ -0,0 +1,132 @@
+using FourierFlows, Printf, Random, Plots
+
+using Random: seed!
+using FFTW: rfft, irfft
+
+import GeophysicalFlows.TwoDNavierStokes
+import GeophysicalFlows.TwoDNavierStokes: energy, enstrophy
+import GeophysicalFlows: peakedisotropicspectrum
+
+dev = CPU()     # Device (CPU/GPU)
+nothing # hide
+
+n, L  = 128, 2π             # grid resolution and domain length
+nothing # hide
+
+# Then we pick the time-stepper parameters
+    dt = 1e-2  # timestep
+nsteps = 4000  # total number of steps
+ nsubs = 20    # number of steps between each plot
+nothing # hide
+
+prob = TwoDNavierStokes.Problem(dev; nx=n, Lx=L, ny=n, Ly=L, dt=dt, stepper="FilteredRK4")
+nothing # hide
+
+sol, cl, vs, gr = prob.sol, prob.clock, prob.vars, prob.grid
+x, y = gr.x, gr.y
+nothing # hide
+
+seed!(1234)
+k₀, E₀ = 6, 0.5
+ζ₀ = peakedisotropicspectrum(gr, k₀, E₀, mask=prob.timestepper.filter)
+TwoDNavierStokes.set_zeta!(prob, ζ₀)
+nothing # hide
+
+heatmap(x, y, vs.zeta,
+         aspectratio = 1,
+              c = :balance,
+           clim = (-40, 40),
+          xlims = (-L/2, L/2),
+          ylims = (-L/2, L/2),
+         xticks = -3:3,
+         yticks = -3:3,
+         xlabel = "x",
+         ylabel = "y",
+          title = "initial vorticity",
+     framestyle = :box)
+
+E = Diagnostic(energy, prob; nsteps=nsteps)
+Z = Diagnostic(enstrophy, prob; nsteps=nsteps)
+diags = [E, Z] # A list of Diagnostics types passed to "stepforward!" will  be updated every timestep.
+nothing # hide
+
+filepath = "."
+plotpath = "./plots_decayingTwoDNavierStokes"
+plotname = "snapshots"
+filename = joinpath(filepath, "decayingTwoDNavierStokes.jld2")
+nothing # hide
+
+if isfile(filename); rm(filename); end
+if !isdir(plotpath); mkdir(plotpath); end
+nothing # hide
+
+get_sol(prob) = Array(prob.sol) # extracts the Fourier-transformed solution
+get_u(prob) = Array(irfft(im*gr.l.*gr.invKrsq.*sol, gr.nx))
+out = Output(prob, filename, (:sol, get_sol), (:u, get_u))
+saveproblem(out)
+nothing # hide
+
+p1 = heatmap(x, y, vs.zeta,
+         aspectratio = 1,
+                   c = :balance,
+                clim = (-40, 40),
+               xlims = (-L/2, L/2),
+               ylims = (-L/2, L/2),
+              xticks = -3:3,
+              yticks = -3:3,
+              xlabel = "x",
+              ylabel = "y",
+               title = "vorticity, t="*@sprintf("%.2f", cl.t),
+          framestyle = :box)
+
+p2 = plot(2, # this means "a plot with two series"
+               label = ["energy E(t)/E(0)" "enstrophy Z(t)/Z(0)"],
+              legend = :right,
+           linewidth = 2,
+               alpha = 0.7,
+              xlabel = "t",
+               xlims = (0, 41),
+               ylims = (0, 1.1))
+
+l = @layout grid(1, 2)
+p = plot(p1, p2, layout = l, size = (900, 400))
+
+startwalltime = time()
+
+anim = @animate for j = 0:Int(nsteps/nsubs)
+
+  log = @sprintf("step: %04d, t: %d, ΔE: %.4f, ΔZ: %.4f, walltime: %.2f min",
+      cl.step, cl.t, E.data[E.i]/E.data[1], Z.data[Z.i]/Z.data[1], (time()-startwalltime)/60)
+
+  if j%(1000/nsubs)==0; println(log) end
+
+  p[1][1][:z] = vs.zeta
+  p[1][:title] = "vorticity, t="*@sprintf("%.2f", cl.t)
+  push!(p[2][1], E.t[E.i], E.data[E.i]/E.data[1])
+  push!(p[2][2], Z.t[Z.i], Z.data[Z.i]/Z.data[1])
+
+  stepforward!(prob, diags, nsubs)
+  TwoDNavierStokes.updatevars!(prob)
+
+end
+
+mp4(anim, "twodturb.mp4", fps=18)
+
+saveoutput(out)
+
+E  = @. 0.5*(vs.u^2 + vs.v^2) # energy density
+Eh = rfft(E)                  # Fourier transform of energy density
+kr, Ehr = FourierFlows.radialspectrum(Eh, gr, refinement=1) # compute radial specturm of `Eh`
+nothing # hide
+
+plot(kr, abs.(Ehr),
+    linewidth = 2,
+        alpha = 0.7,
+       xlabel = "kᵣ", ylabel = "∫ |Ê| kᵣ dk_θ",
+        xlims = (5e-1, gr.nx),
+       xscale = :log10, yscale = :log10,
+        title = "Radial energy spectrum",
+       legend = false)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
